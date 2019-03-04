d72cb8b4bc105754f59ef8672dd6ad7620d53c35
Paterson <a_paterson@tss-hr420-dm12.fc.ca>
2019-03-04 13:21:41 -0500

    updated js and css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (() => {
 
 	// hook up the JS file
-	console.log("fired! rady to jam out");
+	// console.log("fired! rady to jam out");
 
 	function playDrumSound(e) {
 		// e in the brakets is the event object -> 
@@ -12,12 +12,37 @@
 		
 	  	// Try selecting the matching audio element
 		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-		// Set the press key function to immediately go back to the beginning when pressed.
+		
+		
 		// Below stop the js code from running EXIT!! STOP THE CODE HERE
 		if(!audio) { return;}
+		// Set the press key function to immediately go back to the beginning when pressed.
 		audio.currentTime = 0;
 		audio.play();
+
+
+		// Select teh parent div and animate it
+		// Try selecting the matching parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing');
+
+	}
+
+	function removePlayingClass(e) {
+		//listern for a CSS transition to finish and then reset the element
+		//by removing the plaing class
+
+		if (e.propertyName !== "transform") {
+			return;
+		} else {
+			console.log('Do some STUFF!!');
+			e.currentTarget.classList.remove('playing');
+		}
 	}
 	// when the user pushes a key
 	window.addEventListener('keydown', playDrumSound);
+
+	// Create the event 
+	const key = Array.from(document.querySelectorAll(`.key`));
+	key.forEach(key => key.addEventListener('transitionend', removePlayingClass));
 })();
